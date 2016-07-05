b0fc9ac56d45c0ee197db4267de6ee01af470151
Ahmad Saleem <it_saleem@hotmail.com>
2016-07-05 11:53:54 +0300

    change server to wildfly and db connection string

--- build/generated-sources/ap-source-output/entity/User_.java
+++ build/generated-sources/ap-source-output/entity/User_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-19T13:35:53")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-07-05T10:25:05")
 @StaticMetamodel(User.class)
 public class User_ { 
 
